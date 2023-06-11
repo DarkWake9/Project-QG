81658d719fcfa4a6738787ae388fd7de788cebb6
DarkWake9 <vibhavasu2018@gmail.com>
2023-06-11 20:45:49 +0530

    11062023 - 20.45 - UPDATE: ADDED SLURM AND PBS BASH SCRIPTS TO RUN ON CCLUSTER AND PARAMSEVA

---
+++ task0/cchalf2.sh
@@ -0,0 +1,9 @@
+#! /bin/bash -l
+#PBS -o task0ahalf2_1106.out
+#PBS -e task0ahalf2_1106.err
+#PBS -l nodes=1:ppn=32
+#PBS -q long
+
+#cd $PBS_O_WORKDIR
+cd /scratch/shantanu/icecube/Project-QG/task0
+python3 ./task0a_logeq_half1.py
