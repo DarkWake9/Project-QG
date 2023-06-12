1f4ffd5bace7f684d27cb07678ff3b28e50e0fea
DarkWake9 <vibhavasu2018@gmail.com>
2023-06-12 16:55:06 +0530

    12062023 - 16.35 - UPDATE: USE ALL AVAILABLE CPUS

---
+++ task0/linEBphalf2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash                    ## SLURM JOB FOR 2nd HALF
+
+#SBATCH --job-name=half2      ## Name of the job
+#SBATCH --output=linEbhalf2.out    ## Output file
+#SBATCH --error=linEbhalf2.err     ## Error file
+#SBATCH --nodes=1              ## Number of nodes
+#SBATCH --ntasks-per-node=48    ## Number of tasks per node
+#SBATCH --time=23:59:59
+
+## Load the python interpreter
+module load python
+module load conda
+conda activate vibenv
+## Execute the python script and pass the argument/input '90'
+srun python task0c_logeq_logl_mod2_linEb_half2.py
