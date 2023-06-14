63fc7d5c575bcc1e449911dd943170432743dd2b
DarkWake9 <vibhavasu2018@gmail.com>
2023-06-14 18:44:15 +0530

    14062023 - 18.43 - UPDATE: TOO MANY OPEN FILES MULTIPROCESSING

--- task1/task1a_dynesty.py
+++ task1/task1a_dynesty.py
@@ -152,20 +152,26 @@ for grb in GRBs:
 
     #SAMPLING
     nlive = 1024
-
+    
     with dyn.pool.Pool(ncpu, loglike_null, prior_transform_null) as pool0:
         sampler0 = dyn.NestedSampler(loglike_null, prior_transform_null, ndim=5, nlive = nlive, sample='rwalk', bound='multi', pool=pool0)
         sampler0.run_nested(dlogz=0.01)
+        
+    pool0.close()
     dyn.utils.save_sampler(sampler0, 'outputs/sampler_saves/' + grb + '_null_sampler.pkl')
 
     with dyn.pool.Pool(ncpu, loglike_linear, prior_transform_linear) as pool1:
         sampler1 = dyn.NestedSampler(loglike_linear, prior_transform_linear, ndim=6, nlive = nlive, sample='rwalk', bound='multi', pool=pool1)
         sampler1.run_nested(dlogz=0.01)
+        
+    pool1.close()
     dyn.utils.save_sampler(sampler1, 'outputs/sampler_saves/' + grb + '_linear_sampler.pkl')
-
+    
     with dyn.pool.Pool(ncpu, loglike_quad, prior_transform_quadratic) as pool2:
         sampler2 = dyn.NestedSampler(loglike_quad, prior_transform_quadratic, ndim=6, nlive = nlive, sample='rwalk', bound='multi', pool=pool2)
         sampler2.run_nested(dlogz=0.01)
+        
+    pool2.close()
     dyn.utils.save_sampler(sampler2, 'outputs/sampler_saves/' + grb + '_quadratic_sampler.pkl')
     
 
