2de625cee86fd4a203ef41700d009c29ec5d16eb
DarkWake9 <vibhavasu2018@gmail.com>
2023-06-24 14:17:04 +0530

    24042023 - 14.16 - RESOLVED #7 - UPDATE: LOG UNIRFORM PRIOR RANGES START FROM -1 INSTEAD OF -20 PREVIOUSLY

--- task1/task1a_dynesty3.py
+++ task1/task1a_dynesty3.py
@@ -131,9 +131,9 @@ for grb in GRBs:
     eqglinmax = 1e20
     eqquadmin = 1e-20
     eqquadmax = 1e15
-    logeq1min = -20
+    logeq1min = -1
     logeq1max = 20
-    logeq2min = -20
+    logeq2min = -1
     logeq2max = 15
 
 
