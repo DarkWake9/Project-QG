bea0ad47403b391d24a70c423c829aecbaf805e4
DarkWake9 <vibhavasu2018@gmail.com>
2023-10-02 04:35:29 +0530

    02102023 - 04.35 - UPDATE: SLURM FILES AND GENERATOR CODES UPDATED

---
+++ xerr_run_sub.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+
+cd /scratch/vibhavasu.phy.iith/Project-QG/task1/
+chmod +x slurm_liu_sub_xerr_fit_all.sh
+./slurm_liu_sub_xerr_fit_all.sh
+
+cd /scratch/vibhavasu.phy.iith/Project-QG/task2/
+chmod +x slurm_awl_sub_xerr_fit_all.sh
+./slurm_awl_sub_xerr_fit_all.sh
--- xerr_run_sup.sh
+++ xerr_run_sup.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/sh
 
-cd /scratch/vibhavasu.phy.iith/Project-QG/task1/
-chmod +x slurm_liu_sub_xerr_fit_all.sh
-./slurm_liu_sub_xerr_fit_all.sh
-
-cd /scratch/vibhavasu.phy.iith/Project-QG/task2/
-chmod +x slurm_awl_sub_xerr_fit_all.sh
-./slurm_awl_sub_xerr_fit_all.sh
-
 cd /scratch/vibhavasu.phy.iith/Project-QG/task3/
 chmod +x slurm_awl_sup_xerr_fit_all.sh
 ./slurm_awl_sup_xerr_fit_all.sh
