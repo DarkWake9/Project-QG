4c82e18e7437953fd9b14f2e119c3d06a12fcbf8
DarkWake9 <vibhavasu2018@gmail.com>
2023-10-02 04:22:42 +0530

    02102023 - 04.20 - FIX: (#14) ADDED X_ERR BASED FITS; UPDATE: SLURM FILES AND GENERATOR CODES UPDATED

--- xerr_run_all.sh
+++ xerr_run_all.sh
@@ -2,16 +2,16 @@
 
 cd ./task1/
 chmod +x slurm_liu_sub_xerr_fit_all.sh
-sbatch slurm_liu_sub_xerr_fit_all.sh
+./slurm_liu_sub_xerr_fit_all.sh
 
 cd ../task2/
 chmod +x slurm_awl_sub_xerr_fit_all.sh
-sbatch slurm_awl_sub_xerr_fit_all.sh
+./slurm_awl_sub_xerr_fit_all.sh
 
 cd ../task3/
 chmod +x slurm_awl_sup_xerr_fit_all.sh
-sbatch slurm_awl_sup_xerr_fit_all.sh
+./slurm_awl_sup_xerr_fit_all.sh
 
 cd ../task4/
 chmod +x slurm_liu_sup_xerr_fit_all.sh
-sbatch slurm_liu_sup_xerr_fit_all.sh
+./slurm_liu_sup_xerr_fit_all.sh
