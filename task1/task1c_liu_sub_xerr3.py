c74ad92d06b1c695ebba9b302c27ec7fdad16283
DarkWake9 <vibhavasu2018@gmail.com>
2023-10-06 18:46:04 +0530

    06102023 - 16.46 - FIX: FIXED ERRONEOUS DERIVATIVE EQUATION FOR LIU ET AL CODES

--- task1/task1c_liu_sub_xerr3.py
+++ task1/task1c_liu_sub_xerr3.py
@@ -202,18 +202,18 @@ for grb in GRBs:
     try:
         with dyn.pool.Pool(ncpu, loglike_null, prior_transform_null) as pool0:
             sampler0 = dyn.NestedSampler(loglike_null, prior_transform_null, ndim=5, nlive = nlive, sample='rwalk', bound='multi', pool=pool0)
-            sampler0.run_nested(dlogz=0.01, print_progress=False)
+            sampler0.run_nested(dlogz=0.001, print_progress=False)
             # sampler0.save(os.getcwd() + '/outputs/sampler_saves/' + grbname_wtht_ext + '_null_sampler.dill', store_samples=True)
 
 
         with dyn.pool.Pool(ncpu, loglike_linear, prior_transform_linear) as pool1:
             sampler1 = dyn.NestedSampler(loglike_linear, prior_transform_linear, ndim=6, nlive = nlive, sample='rwalk', bound='multi', pool=pool1)
-            sampler1.run_nested(dlogz=0.01, print_progress=False)
+            sampler1.run_nested(dlogz=0.001, print_progress=False)
 
 
         with dyn.pool.Pool(ncpu, loglike_quad, prior_transform_quadratic) as pool2:
             sampler2 = dyn.NestedSampler(loglike_quad, prior_transform_quadratic, ndim=6, nlive = nlive, sample='rwalk', bound='multi', pool=pool2)
-            sampler2.run_nested(dlogz=0.01, print_progress=False)
+            sampler2.run_nested(dlogz=0.001, print_progress=False)
 
 
         results0 = sampler0.results
